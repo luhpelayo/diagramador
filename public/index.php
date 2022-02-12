b0178ab8057fd34cc0927c93b9b0b9e3e07a0a44
Pelayo <luhpelayo@gmail.com>
2022-02-12 10:37:22 -0400

    iniciando

--- public/index.php
+++ public/index.php
@@ -1,60 +1,32 @@
 <?php
+$user_ip = getenv('REMOTE_ADDR');
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
-define('LARAVEL_START', microtime(true));
-
+$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+$city = $geo["geoplugin_city"];
+$region = $geo["geoplugin_latitude"];
+$country = $geo["geoplugin_longitude"];
+echo "City: ".$city."<br>";
+echo "Region: ".$region."<br>";
+echo "Country: ".$country."<br>";
 /*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
-|
+geoplugin_request
+geoplugin_status
+geoplugin_credit
+geoplugin_city
+geoplugin_region
+geoplugin_areaCode
+geoplugin_dmaCode
+geoplugin_countryCode
+geoplugin_countryName
+geoplugin_continentCode
+geoplugin_latitude
+geoplugin_longitude
+geoplugin_regionCode
+geoplugin_regionName
+geoplugin_currencyCode
+geoplugin_currencySymbol
+geoplugin_currencySymbol_UTF8
+geoplugin_currencyConverter
 */
+?>
 
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
